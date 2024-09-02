d47dc95a0a2b887ed3a999bf736362aaf848236f
Israel <israel.toledo@outlook.es>
2024-09-02 11:26:24 -0600

    Se añadió mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express'
 import routes from './routes/index.js'
+import mongoose from 'mongoose'
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis',{
+    useNewUrlParser : true
+})
+
+
 const app = express();
 
 app.listen(5000);
